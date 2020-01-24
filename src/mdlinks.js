fad1a35d46d08b0fd8dd787c3b3e6d2470f4ee40
vanesagarro <vanesagarro97@gmail.com>
2020-01-24 18:31:52 -0500

    test

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,2 +1,8 @@
-const isAbsolute = () => 'hola';
+/* eslint-disable global-require */
+/* eslint-disable no-unused-vars */
+const isAbsolute = (input) => {
+  const path = require('path');
+  return (path.isAbsolute(input));
+};
+
 module.exports = isAbsolute;
--- test/mdlinks.test.js
+++ test/mdlinks.test.js
@@ -1,5 +1,12 @@
 const isAbsolute = require('../src/mdlinks');
 
-test('Funcion que retorna si la ruta es absoluta o no', () => {
-  expect(isAbsolute()).toBe('hola');
+describe('funcion que retorna si una ruta es absoluta', () => {
+  it('debería ingresar una ruta absoluta y retorna true', () => {
+    const inputAbsolute = '/test/demo_path.js';
+    expect(isAbsolute(inputAbsolute)).toEqual(true);
+  });
+  it('debería ingresar una ruta relativa y retorna false', () => {
+    const inputRelative = 'test/demo_path.js';
+    expect(isAbsolute(inputRelative)).toEqual(false);
+  });
 });
