06c457999890f7507a8d296e9757c9f2f869a2df
vanesagarro <vanesagarro97@gmail.com>
2020-01-27 19:11:18 -0500

    leer carpeta

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,8 +1,20 @@
-/* eslint-disable global-require */
-/* eslint-disable no-unused-vars */
-const isAbsolute = (input) => {
-  const path = require('path');
-  return (path.isAbsolute(input));
-};
+const path = require('path');
+const fs = require('fs');
 
-module.exports = isAbsolute;
+const isAbsolute = (input) => (path.isAbsolute(input));
+const convertToAbsolute = (input) => (path.resolve(input));
+
+
+const readDir = (dir) => {
+  fs.readdir(dir, (err, archivos) => {
+    if (err) {
+      // eslint-disable-next-line no-undef
+      onError(err);
+      return;
+    }
+    console.log(archivos);
+  });
+};
+module.exports = {
+  isAbsolute, convertToAbsolute, readDir,
+};
--- test/mdlinks.test.js
+++ test/mdlinks.test.js
@@ -1,12 +1,24 @@
-const isAbsolute = require('../src/mdlinks');
+const mdlinks = require('../src/mdlinks');
 
 describe('funcion que retorna si una ruta es absoluta', () => {
   it('debería ingresar una ruta absoluta y retorna true', () => {
-    const inputAbsolute = '/test/demo_path.js';
-    expect(isAbsolute(inputAbsolute)).toEqual(true);
+    const inputAbsolute = '/home/vanesa/Escritorio/LIM011-fe-md-links/README.md';
+    expect(mdlinks.isAbsolute(inputAbsolute)).toEqual(true);
   });
   it('debería ingresar una ruta relativa y retorna false', () => {
-    const inputRelative = 'test/demo_path.js';
-    expect(isAbsolute(inputRelative)).toEqual(false);
+    const inputRelative = '../LIM011-fe-md-links/README.md';
+    expect(mdlinks.isAbsolute(inputRelative)).toEqual(false);
+  });
+});
+describe('funcion que convierte una ruta relativa en una ruta absoluta', () => {
+  it('Debería ingresar una ruta relativa y convertirla a absoluta', () => {
+    const inputRelative = '../LIM011-fe-md-links/README.md';
+    expect(mdlinks.convertToAbsolute(inputRelative)).toEqual('/home/vanesa/Escritorio/LIM011-fe-md-links/README.md');
+  });
+});
+describe('funcion que lee el contenido de un arhivo', () => {
+  it('Deberia ingresar un archivo y leer el contenido', () => {
+    const inputFile = 'src';
+    expect(mdlinks.readDir(inputFile)).toEqual('[mdlinks.js]');
   });
 });
