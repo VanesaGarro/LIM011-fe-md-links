6214e4305dcb1b998d98a6b7f6c01de32d92ab57
VanesaGarro <vanesagarro97@gmail.com>
2020-02-05 02:20:02 -0500

    refactorizar

--- src/index.js
+++ src/index.js
@@ -1,22 +1,73 @@
 /* eslint-disable object-shorthand */
-const marked = require('marked');
+const path = require('path');
 const fs = require('fs');
+const marked = require('marked');
+const fetch = require('node-fetch');
 
-const extraerLinks = (arrayMd) => {
-  arrayMd.forEach((element) => {
-    const readfile = fs.readFileSync(element, 'utf8');
-    arrayMd.push(readfile);
-    const renderer = new marked.Renderer();
-    renderer.link = (href, title, text) => {
-      arrayMd.push({ href: href, text: text, path: element });
-    };
-    marked(readfile, { renderer });
+const isAbsolute = (ruta) => (path.isAbsolute(ruta));
+const getPaths = (ruta) => fs.readdirSync(ruta).map((file) => path.resolve(ruta, file));
+console.log(getPaths('C:/Users/Vanesa/Desktop/LIM011-fe-md-links/prueba'));
+const isMd = (fileMd) => path.extname(fileMd) === '.md';
+const isFile = (file) => fs.statSync(file).isFile();
+const isDirectory = (directory) => fs.statSync(directory).isDirectory();
+const readFile = (file) => fs.readFileSync(file, 'utf8');
+const searchMdFiles = (ruta) => {
+  let arrayMd = [];
+  if (isFile(ruta)) {
+    if (isMd(ruta)) {
+      const routeAbsolute = path.resolve(ruta);
+      arrayMd = arrayMd.concat(routeAbsolute);
+      return arrayMd;
+    }
+    return [];
+  }
+  getPaths(ruta).forEach((elemento) => {
+    arrayMd = arrayMd.concat(searchMdFiles(elemento));
   });
   return arrayMd;
 };
+const extraerLinks = (ruta) => {
+  const renderer = new marked.Renderer();
+  const links = [];
+  searchMdFiles(ruta).forEach((file) => {
+    renderer.link = (href, title, text) => {
+      links.push({ href, text, file });
+    };
+    marked(readFile(file), { renderer });
+  });
+  return links;
+};
+const validateLinks = (ruta) => {
+  const array = [];
+  extraerLinks(ruta).forEach((element) => {
+    fetch(element.href)
+      .then((response) => {
+        if ((response.status >= 200) && (response.status <= 399)) {
+          array.push({
+            status: response.status,
+            message: 'OK',
+          });
+        } else {
+          array.push({
+            status: response.status,
+            message: 'FAIL',
+          });
+        }
+        console.log(array);
 
-
-module.exports = { extraerLinks };
-console.log(extraerLinks(['/home/vanesa/Escritorio/LIM011-fe-md-links/prueba/carpeta2/read.md',
-  '/home/vanesa/Escritorio/LIM011-fe-md-links/prueba/probando.md']));
-// console.log(probar(['a', 'b', 'c']));
+        return array;
+      });
+  });
+};
+// console.log(readFile('/home/vanesa/Escritorio/LIM011-fe-md-links/README.md'));
+// console.log(readDirectory('prueba'));
+// console.log(isMd('README.md'));
+// console.log(convertToAbsolute('readme2.md'));
+// console.log(isFile('holi.html'));
+// console.log(isDirectory('prueba'));
+// console.log(searchMdFiles('C:/Users/Vanesa/Desktop/LIM011-fe-md-links/README.md'));
+// console.log(extraerLinks('C:/Users/Vanesa/Desktop/LIM011-fe-md-links/prueba'));
+// console.log(validateLinks('/home/vanesa/Escritorio/LIM011-fe-md-links/README.md'));
+module.exports = {
+  isAbsolute, readFile, isFile, isDirectory, extraerLinks, validateLinks,
+};
--- test/mdlinks.test.js
+++ test/mdlinks.test.js
@@ -2,7 +2,7 @@ const mdlinks = require('../src/mdlinks');
 
 describe('funcion que retorna si una ruta es absoluta', () => {
   it('debería ingresar una ruta absoluta y retorna true', () => {
-    const inputAbsolute = '/home/vanesa/Escritorio/LIM011-fe-md-links/README.md';
+    const inputAbsolute = 'C:/Users/Vanesa/Desktop/LIM011-fe-md-links/README.md';
     expect(mdlinks.isAbsolute(inputAbsolute)).toEqual(true);
   });
   it('debería ingresar una ruta relativa y retorna false', () => {
@@ -10,15 +10,9 @@ describe('funcion que retorna si una ruta es absoluta', () => {
     expect(mdlinks.isAbsolute(inputRelative)).toEqual(false);
   });
 });
-describe('funcion que convierte una ruta relativa en una ruta absoluta', () => {
-  it('Debería ingresar una ruta relativa y convertirla a absoluta', () => {
-    const inputRelative = '/hola.html';
-    expect(mdlinks.convertToAbsolute(inputRelative)).toEqual('/home/vanesa/Escritorio/LIM011-fe-md-links/README.md');
-  });
-});
-describe('funcion que lee el contenido de un arhivo', () => {
-  it('Deberia ingresar un archivo y leer el contenido', () => {
-    const inputFile = 'src';
-    expect(mdlinks.readDirectory(inputFile)).toEqual(['mdlinks.js']);
+describe('', () => {
+  it('Debería ingresar una ruta ', () => {
+    const inputRelative = 'C:/Users/Vanesa/Desktop/LIM011-fe-md-links/README.md';
+    expect(mdlinks.extraerLinks(inputRelative)).toEqual('C:/Users/Vanesa/Desktop/LIM011-fe-md-links/README.md');
   });
 });
