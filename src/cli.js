fbcd4172171f2eee942ce64ad5be1a247b58a992
VanesaGarro <vanesagarro97@gmail.com>
2020-02-19 23:38:24 -0500

    corrige cli

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ const mdLinks = require('./mdlinks-cli');
 
 const path = process.argv[2];
 const options = {
-  validate: process.argv[3],
-  stats: process.argv[4],
+  stats: process.argv[3],
+  validate: process.argv[4],
 };
 mdLinks.cli(path, options).then((res) => console.log(res)).catch(() => console.log('ingrese una ruta'));
--- src/mdlinks-cli.js
+++ src/mdlinks-cli.js
@@ -2,26 +2,27 @@ const stats = require('./stats');
 const mdlinks = require('./mdlinks');
 
 const cli = (path, options) => {
-  if (options.validate === '--validate' && options.stats === '--stats') {
+  if (options.stats === '--stats' && options.validate === '--validate') {
     return mdlinks.mdLinks(path, { validate: true }).then((data) => {
       let sValidate = '';
       sValidate += `Total: ${stats.totalStats(data)}\nUniques: ${stats.uniqueStats(data)}\nBroken: ${stats.brokenStats(data)}`;
       return sValidate;
     });
   }
-  if (options.validate === '--validate') {
+  if (options.stats === '--stats') {
+    return mdlinks.mdLinks(path, { validate: true }).then((data) => {
+      let stat = '';
+      stat += `Total: ${stats.totalStats(data)}\nUniques: ${stats.uniqueStats(data)}`;
+      return stat;
+    });
+  }
+  if (options.stats === '--validate') {
     return mdlinks.mdLinks(path, { validate: true }).then((data) => {
       let validate = '';
       data.forEach((element) => {
         validate += `${element.file} ${element.href} ${element.statusText} ${element.status} ${element.text} \n`;
       });
       return validate;
-    });
-  } if (options.validate === '--stats') {
-    return mdlinks.mdLinks(path, { validate: true }).then((data) => {
-      let stat = '';
-      stat += `Total: ${stats.totalStats(data)}\nUniques: ${stats.uniqueStats(data)}`;
-      return stat;
     });
   }
 
